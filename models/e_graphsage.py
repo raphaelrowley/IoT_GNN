ba6ab04469745d576250b53380d60ed86251eaa1
moritzhemsing <moritz.hemsing@gmx.de>
2025-11-15 17:07:32 -0500

    Implementation of E-GraphSAGE

--- models/e_graphsage.py
+++ models/e_graphsage.py
@@ -3,51 +3,100 @@ from data import IoTDataset
 
 import dgl
 
-# class E_GraphSage_Layer(nn.Module):
-#
-#     def __init__(self, in_channels, out_channels, **kwargs):
-#         super(E_GraphSage_Layer, self).__init__()
-#
-#         self.neighborhood_sampler = geo_nn.MessagePassing(
-#             aggr='mean',
-#             flow='source_to_target',
-#
-#         )
+class E_GraphSAGE(nn.Module):
 
+    def __init__(self, numLayers, dim_node_embed, num_edge_attr, num_classes, dropout=0.2):
+        super(E_GraphSAGE, self).__init__()
 
-class E_GraphSage_Layer(geo_nn.MessagePassing):
-
-    def __init__(self, in_channels, out_channels):
-        super(E_GraphSage_Layer, self).__init__(
-            aggr='mean'
+        # Use the given number of E_GraphSAGE_Layers, dropout between each layer
+        self.graphsage = nn.Sequential(
+            E_GraphSAGE_Layer(num_edge_attr=num_edge_attr,
+                              dim_node_embed=num_edge_attr,
+                              dim_new_node_embed=dim_node_embed,
+                              dropout=(dropout if numLayers > 1 else None),
+                              ),
         )
+        for k in range(numLayers-2):
+            self.graphsage.append(
+                E_GraphSAGE_Layer(num_edge_attr=num_edge_attr, dim_node_embed=dim_node_embed,
+                                  dim_new_node_embed=dim_node_embed, dropout=dropout),
+            )
+        if numLayers > 1:
+            self.graphsage.append(
+                E_GraphSAGE_Layer(num_edge_attr=num_edge_attr, dim_node_embed=dim_node_embed,
+                                  dim_new_node_embed=dim_node_embed, dropout=None),
+            )
+
+        dim_output = 1 if (num_classes == 2) else num_classes
+        self.mlp = nn.Sequential(
+            nn.Linear(2*dim_node_embed, dim_output),
+        )
+        if dim_output > 1:
+            self.mlp.append(nn.Softmax(dim=-1))
 
     def forward(self, graph):
-        edge_index = graph.edge_index
-        edge_attr = torch.cat((graph.edge_attr, graph.edge_attr), dim=-1)
+        graph = self.graphsage(graph)
+        graph.apply_edges(lambda edges:
+                          {'edge_pred' : torch.cat((edges.src['node_attr'], edges.dst['node_attr']), dim=-1)}
+                          )
+        graph.edata['edge_pred'] = self.mlp(graph.edata['edge_pred'])
 
-        print('EDGE INDEX', edge_index.shape)
-        print('EDGE ATTR', edge_attr.shape)
-        node_attr = graph.node_attr
-        print('NODE ATTR', node_attr.shape)
-        # print(edge_attr)
+        return graph
 
-        temp = self.propagate(edge_index, edge_attr=edge_attr, x=node_attr)
-        print(self.edge_updater(edge_index, edge_attr=edge_attr))
-        print(temp.shape)
 
-    def message(self, x_j):
-        print("TEST")
-        print(x_j.shape)
-        return super(E_GraphSage_Layer, self).message(x_j)
+class E_GraphSAGE_Layer(nn.Module):
+
+    def __init__(self, num_edge_attr, dim_node_embed, dim_new_node_embed, dropout=0.2):
+        super(E_GraphSAGE_Layer, self).__init__()
+
+        self.linear = nn.Linear(in_features=(dim_node_embed+num_edge_attr), out_features=dim_new_node_embed, bias=False)
+        self.relu = nn.ReLU()
+
+        self.dropout = None
+        if dropout is not None:
+            self.dropout = nn.Dropout(dropout)
+
+    def forward(self, graph):
+
+        # Line (4): Aggregate Edge Features
+        graph.update_all(
+            dgl.function.copy_e(e='edge_attr', out='m'),
+            dgl.function.mean(msg='m', out='h_Nv')
+        )
+
+        # Line (5)
+        graph.ndata['node_attr'] = self.linear(torch.cat((graph.ndata['node_attr'], graph.ndata['h_Nv']), dim=-1))
+        graph.ndata['node_attr'] = self.relu(graph.ndata['node_attr'])
+        if self.dropout is not None:
+            graph.ndata['node_attr'] = self.dropout(graph.ndata['node_attr'])
+        del graph.ndata['h_Nv']
+
+        return graph
 
 
 def test():
-    train_data = IoTDataset()
-    nx_graph = pyg.utils.to_networkx(train_data)
-    dgl_graph = dgl.from_networkx(nx_graph)
+    train_data = IoTDataset(version=1)
+    pyg_graph = train_data[0]
+    nx_graph = torch_geometric.utils.to_networkx(
+        pyg_graph,
+        to_undirected=False,
+        node_attrs=["node_attr"],
+        edge_attrs=["edge_attr", "edge_label"]
+    )
+    dgl_graph = dgl.from_networkx(
+        nx_graph,
+        node_attrs=["node_attr"],
+        edge_attrs=["edge_attr", "edge_label"]
+    )
     print(dgl_graph)
-    layer = E_GraphSage_Layer(train_data.num_features, 64)
-    layer.forward(train_data.__getitem__(0))
+
+    temp = E_GraphSAGE(numLayers=2, num_edge_attr=8, dim_node_embed=128, num_classes=5)
+    temp.forward(dgl_graph)
 
 test()
+
+
+# TODO
+#   – Maybe do copy.deepcopy() first in forward or ensure this is done in training?
+#   – Otherwise, we modify everything etc.
+#   – Prevent target leakage, do not provide labels!
