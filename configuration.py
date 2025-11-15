efd7a322705ce52a26258e44e0055572dae2e582
moritzhemsing <moritz.hemsing@gmx.de>
2025-11-15 18:18:37 -0500

    Removed torch_geometric

--- configuration.py
+++ configuration.py
@@ -1,15 +1,17 @@
-import torch, os, torch_geometric.utils, torch_geometric.transforms
+import torch, os
 import torch.nn as nn
 import numpy as np
-import torch_geometric.nn as geo_nn
+
 import pandas as pd
 import networkx as nx
 import sklearn as sk
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
-from torch_geometric.data import Data
 
 import sklearn.preprocessing
 import sklearn.model_selection
 
 import ipaddress
+
+import dgl
+import pickle
