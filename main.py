27801a4ec32308cb7d65a6320526b2773157d007
moritzhemsing <moritz.hemsing@gmx.de>
2025-11-25 15:28:30 -0500

    Compact config of dataset, changed dimensions

--- main.py
+++ main.py
@@ -7,38 +7,45 @@ from tester import ModelTester
 
 
 def main():
-    multiclass = True
+    dataset_config = {
+        'multiclass': True,
+        'dataset': 'NF-ToN-IoT',
+        'version': 1,
+        'randomize_source_ip': True,
+        'relabel_nodes': False,
+    }
+
     print('\rLoading and preprocessing data…', end='')
-    train_data = IoTDataset(version=1, multiclass=multiclass)
-    val_data = IoTDataset(version=1, multiclass=multiclass, split='val')
-    test_data = IoTDataset(version=1, multiclass=multiclass, split='test')
+    train_data = IoTDataset(**dataset_config)
+    val_data = IoTDataset(**dataset_config, split='val')
+    test_data = IoTDataset(**dataset_config, split='test')
 
     print('\rInitializing model…', end='')
     model = e_graphsage.E_GraphSAGE(numLayers=2,
-                                    dim_node_embed=64,
+                                    dim_node_embed=128,
                                     num_edge_attr=train_data.num_features,
                                     num_classes=len(train_data.classes)
                                     )
 
     model2 = fnn_model.TestFNN(num_hidden_layers=3,
-                               hidden_layer_widths=[64, 64, 128],  # Should be approximately comparable to EGS
+                               hidden_layer_widths=[128, 128, 256],  # Should be approximately comparable to EGS
                                num_edge_attr=train_data.num_features,
                                num_classes=len(train_data.classes),
                                )
 
     training_config = {
-        'num_epochs': 100,
+        'num_epochs': 5000,             # TODO Increase?
         'lr': 1e-3,
         'gpu': False,
-        'lr_sched_factor': np.sqrt(10),
-        'lr_sched_patience': 10,
+        'lr_sched_factor': np.sqrt(10),     # TODO or set to 1 first?
+        'lr_sched_patience': 100,        # TODO maybe scale appropriately
     }
 
     print('\rStarting training…', end='')
     trainer = ModelTrainer(training_config, train_data, val_data)
 
-    trainer.train_model(model, False)
-    trainer.train_model(model2, False)
+    trainer.train_model(model, True)
+    trainer.train_model(model2, True)
 
     tester = ModelTester(test_data, False)
     tester.test_model(model)
