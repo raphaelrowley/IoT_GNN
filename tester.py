79b085ec4002ee8b77da733017d3f5242d04dfac
raphaelrowley <rrpsid@gmail.com>
2025-11-30 15:55:25 -0500

    Now computing "binary metrics".

--- tester.py
+++ tester.py
@@ -1,5 +1,5 @@
 from configuration import *
-
+import stats_util
 
 class ModelTester:
     def __init__(self, test_data, use_gpu):
@@ -98,7 +98,39 @@ class ModelTester:
                 # Confusion Matrix.
                 conf_matrix = sklearn.metrics.confusion_matrix(y_true=target, y_pred=y_pred, labels=self.test_data.classes)
 
-                
+                # #########################
+                # Extract TP, FP, FN, TN
+                # #########################
+                # True benign and predicted benign
+                num_classes = self.test_data.classes
+                tn = 0
+                fn = 0
+                fp = 0
+                tp = 0
+                # Iterate over rows
+                for i in range(len(conf_matrix)):
+                    # True Label = Benign
+                    if i == 0:
+                        tn = conf_matrix[i][i]
+                        # Calculate FP (sum of the other elements of row 1)
+                        for j in range(len(conf_matrix[i])-1):
+                            fp += conf_matrix[i][j+1]
+                    # True Label = An Attack Class
+                    else:
+                        # Rest of first column is false negative
+                        fn += conf_matrix[i][0]
+                        # Rest of the row is part of TP
+                        for j in range(len(conf_matrix[i])-1):
+                            tp += conf_matrix[i][j+1]
+
+                total_samples = tn + tp + fn + fp
+
+                bin_acc = stats_util.get_accuracy(tn, tp, fn, fp)
+                bin_bal_acc = stats_util.get_balanced_accuracy(tn, tp, fn, fp)
+                bin_far = stats_util.get_far(tn, tp, fn, fp)
+                bin_recall = stats_util.get_recall(tn, tp, fn, fp)
+                bin_precision = stats_util.get_precision(tn, tp, fn, fp)
+                bin_f1 = stats_util.get_f1_score(tn, tp, fn, fp)
 
                 # Update progress report
                 for key in progress_reports.keys():
@@ -122,6 +154,23 @@ class ModelTester:
                 # Confusion matrix
                 report_text.append("Confusion Matrix:\n")
                 report_text.append(str(conf_matrix) + "\n\n")
+
+                report_text.append(f"= Binary Metrics =\n")
+                report_text.append(f"TN, TP, FN, FP are derived from the confusion matrix.\n")
+                report_text.append(f"Benign is the negative class. The attack classes are positive.\n")
+                print("Total Samples:", total_samples)
+                report_text.append(f"Total Samples: {total_samples}\n")
+                report_text.append(f"TN: {tn}\n")
+                report_text.append(f"TP: {tp}\n")
+                report_text.append(f"FN: {fn}\n")
+                report_text.append(f"FP: {fp}\n\n")
+
+                report_text.append(f"Accuracy: {bin_acc:.6f}\n")
+                report_text.append(f"Balanced Accuracy: {bin_bal_acc:.6f}\n")
+                report_text.append(f"FAR: {bin_far:.6f}\n")
+                report_text.append(f"Recall: {bin_recall:.6f}\n")
+                report_text.append(f"Precision: {bin_precision:.6f}\n")
+                report_text.append(f"F1-score: {bin_f1:.6f}\n")
 
                 # Convest report to text, print it and write to file.
                 report_text = "".join(report_text)
