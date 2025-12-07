3a4fd29dcdc28c948699c2fba3aa814d566b046a
raphaelrowley <rrpsid@gmail.com>
2025-12-06 23:36:03 -0500

    Some cleaning of the main function.

--- main.py
+++ main.py
@@ -17,8 +17,11 @@ def main():
 
     print('\rLoading and preprocessing data…', end='')
     train_data = IoTDataset(**dataset_config)
+    print("\rDone processing train.", end='')
     val_data = IoTDataset(**dataset_config, split='val')
+    print("\rDone processing val.", end='')
     test_data = IoTDataset(**dataset_config, split='test')
+    print("\rDone processing test.", end='')
 
     print('\rInitializing models…', end='')
     model = e_graphsage.E_GraphSAGE(numLayers=2,
@@ -32,12 +35,13 @@ def main():
                                num_edge_attr=train_data.num_features,
                                num_classes=len(train_data.classes),
                                )
-
-    # model3 = e_graphsage_hembed.E_GraphSAGE_hEmbed(numLayers=2,
-    #                                                dim_node_embed=96,       # Approximately equal parameter count as EGS
-    #                                                num_edge_attr=train_data.num_features,
-    #                                                num_classes=len(train_data.classes)
-    #                                                )
+    
+    # DIDS inspired model.
+    model11 = fnn_model.TestFNN(num_hidden_layers=5,
+                               hidden_layer_widths=[18, 36, 72, 144, 256],
+                               num_edge_attr=train_data.num_features,
+                               num_classes=len(train_data.classes),
+                               )
 
     model3 = enhanced_e_graphsage.Enhanced_E_GraphSAGE(numLayers=2,
                                                        dim_node_embed=128,
@@ -68,19 +72,19 @@ def main():
                                                        num_classes=len(train_data.classes),
                                                        attention=True, gating=False, residual=False
                                                        )
-    model7 = enhanced_e_graphsage.Enhanced_E_GraphSAGE(numLayers=2,
+    model8 = enhanced_e_graphsage.Enhanced_E_GraphSAGE(numLayers=2,
                                                        dim_node_embed=128,
                                                        num_edge_attr=train_data.num_features,
                                                        num_classes=len(train_data.classes),
                                                        attention=False, gating=True, residual=False
                                                        )
-    model8 = enhanced_e_graphsage.Enhanced_E_GraphSAGE(numLayers=2,
+    model9 = enhanced_e_graphsage.Enhanced_E_GraphSAGE(numLayers=2,
                                                        dim_node_embed=128,
                                                        num_edge_attr=train_data.num_features,
                                                        num_classes=len(train_data.classes),
                                                        attention=False, gating=False, residual=True
                                                        )
-    model9 = enhanced_e_graphsage.Enhanced_E_GraphSAGE(numLayers=2,
+    model10 = enhanced_e_graphsage.Enhanced_E_GraphSAGE(numLayers=2,
                                                        dim_node_embed=128,
                                                        num_edge_attr=train_data.num_features,
                                                        num_classes=len(train_data.classes),
@@ -88,13 +92,13 @@ def main():
                                                        )
 
     print('\r' + ' ' * 50 + '\r', end='')
-    for model_t in [model, model2, model3, model4, model5, model6, model7, model8, model9]:
-        print(f'Number of learnable parameters in {model_t.id}: {sum(p.numel() for p in model_t.parameters() if p.requires_grad)}')
+    #for model_t in [model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11]:
+    #    print(f'Number of learnable parameters in {model_t.id}: {sum(p.numel() for p in model_t.parameters() if p.requires_grad)}')
 
     training_config = {
-        'num_epochs': 100,             # TODO Increase? # 5000
+        'num_epochs': 300,             # TODO Increase? # 5000
         'lr': 1e-3,
-        'gpu': False,
+        'gpu': True,
         'lr_sched_factor': np.sqrt(10),     # TODO or set to 1 first?
         'lr_sched_patience': 100,        # TODO maybe scale appropriately
     }
@@ -102,20 +106,20 @@ def main():
     print('\rStarting training…', end='')
     trainer = ModelTrainer(training_config, train_data, val_data)
 
-    trainer.train_model(model, True)
-    trainer.train_model(model2, True)
-    trainer.train_model(model3, True)
-    trainer.train_model(model4, True)
-    trainer.train_model(model5, True)
-    trainer.train_model(model6, True)
-    trainer.train_model(model7, True)
-    trainer.train_model(model8, True)
-    trainer.train_model(model9, True)
+    #trainer.train_model(model, True)
+    #trainer.train_model(model2, True)
+    #trainer.train_model(model3, True)
+    #trainer.train_model(model4, True)
+    #trainer.train_model(model5, True)
+    #trainer.train_model(model6, True)
+    trainer.train_model(model, False)
+    #trainer.train_model(model8, True)
+    #trainer.train_model(model9, True)
 
     tester = ModelTester(test_data, False)
+    #tester.test_model(model)
+    #tester.test_model(model2)
     tester.test_model(model)
-    tester.test_model(model2)
-    tester.test_model(model3)
 
 
 if __name__ == "__main__":
